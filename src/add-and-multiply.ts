d72634ef3f50700f5e70c433ae9e9fbbbeb049eb
Albert Still <alberts@spotify.com>
2025-05-20 21:02:13 +1000

    --wip--

--- src/add-and-multiply.ts
+++ src/add-and-multiply.ts
@@ -1,9 +1,9 @@
-import { add as addNumbers, times } from './calculator';
+import { add, times } from './calculator';
 
 export default function addAndMultiply(
   numberOne: number,
   numberTwo: number,
   numberThree: number,
 ): number {
-  return times(addNumbers(numberOne, numberTwo), numberThree);
+  return times(add(numberOne, numberTwo), numberThree);
 }
